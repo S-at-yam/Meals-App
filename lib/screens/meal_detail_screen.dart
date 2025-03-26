de6d0b19b13402f57af559809cbc4eafd4ed6bc1
Satyam Kumar <Satyam.Kumar@winjit.com>
2025-03-26 17:30:02 +0530

    new improvemnets in meal detail screen

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -8,11 +8,62 @@ class MealDetailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text(meal.title)),
-      body: Image.network(
-        meal.imageUrl,
-        height: 300,
-        fit: BoxFit.cover,
-        width: double.infinity,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.network(
+              meal.imageUrl,
+              height: 300,
+              fit: BoxFit.cover,
+              width: double.infinity,
+            ),
+            const SizedBox(height: 8),
+            Text(
+              'Ingredients',
+
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                color: Theme.of(context).colorScheme.primary,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 8),
+            for (final ingredient in meal.ingredients)
+              Container(
+                padding: EdgeInsets.fromLTRB(5, 0, 5, 0),
+                margin: EdgeInsets.only(left: 40, right: 20),
+                width: double.infinity,
+                child: Text(
+                  '- $ingredient',
+                  textAlign: TextAlign.left,
+                  style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.onSurfaceVariant,
+                  ),
+                ),
+              ),
+            const SizedBox(height: 8),
+            Text(
+              'Steps',
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                color: Theme.of(context).colorScheme.primary,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(height: 8),
+            for (final step in meal.steps)
+              Container(
+                padding: EdgeInsets.all(5),
+                width: double.infinity,
+                margin: EdgeInsets.only(left: 40, right: 20),
+                child: Text(
+                  '- $step',
+                  textAlign: TextAlign.left,
+                  style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.onSurfaceVariant,
+                  ),
+                ),
+              ),
+          ],
+        ),
       ),
     );
   }
